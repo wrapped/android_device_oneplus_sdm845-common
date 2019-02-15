bba53d5b5c2099f7ea4fdc1de5c7672aa946aa1b
wrapped <ddroidx@gmail.com>
2019-02-15 12:28:41 -0600

    sdm845: add hidl.android.base

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -9,3 +9,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0-java
